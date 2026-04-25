e163ba0b23104fee872dcd1331abcf68af88dc4f
rafaorca <souzajulya823@gmail.com>
2026-04-25 11:35:12 -0300

    Exercicio aula 1

---
+++ aula-1/Main (1).java
@@ -0,0 +1,46 @@
+
+
+import java.util.Scanner;
+	public static void main(String[] args) {
+	
+    Scanner lojinha = new Scanner (System.in);
+    double precoAbacaxi = 7.30;
+    int quantidadeAbacaxi = lojinha.nextInt();
+    double precoMaca = 2.00;
+    int quantidadeMaca = lojinha.nextInt();
+    double precoPera = 3.00;
+    int quantidadePera = lojinha.nextInt();
+    double valorCompra = precoAbacaxi*quantidadeAbacaxi + precoMaca*quantidadeMaca + precoPera*quantidadePera;
+
+		System.out.println(valorCompra);
+		new Scanner(System.in);
+		
+		
+		// comparação e ramificação
+	
+		int minhaIdade = 17;
+		int profIdade = 22;
+		boolean SouMaisNovoQueOProf = minhaIdade < profIdade;
+		System.out.println(SouMaisNovoQueOProf);
+		
+		boolean estaChovendo = true;
+		boolean estaRelampejando = true;
+		boolean naoVouSairDeCasa = estaChovendo && estaRelampejando;
+		
+		
+		
+		int idadePA = 18;
+		int idadePB = 12;
+		boolean mesmaIdadeAB = idadePA == idadePB;
+		
+		// correção
+		boolean mesmaIdade = idadeA == idadePB
+	boolean maiorQue18 = idadeA > 18;
+	boolean primeiraComparação = mesmaIdade && maiorIdade
+	
+	boolean idadesDiferentes = idadeA != 
+	boolean menorQue12 = idadeB < 12
+	
+
+	}
+
